691dce2bac45da1d40f8ac6ea9d084ede34d52fd
masdn <masdn14@gmail.com>
2026-03-22 22:10:27 -0600

    fixed flattening issue, doing some planning

--- pipeline.py
+++ pipeline.py
@@ -3,42 +3,64 @@ Download and import the training and test images from MNIST and Fashion MNIST. T
 data are required to be kept in NumPy array format. Complete the following tasks on both of the datasets.
 '''
 
-#%%
+#%% Libs
 import numpy as np
 import matplotlib.pyplot as plt
 import idx2numpy as idx
 from tensorflow.keras.datasets import mnist, fashion_mnist
 
-#train_image_file = './datasets/mnist_images.idx'
-#train_label_file = './datasets/mnist_labels.idx'
-#train_images = idx.convert_from_file(train_image_file)
-#train_labels = idx.convert_from_file(train_label_file)
+from sklearn.preprocessing import StandardScaler()
+
+from sklearn.metrics import accuracy_score
+
+#%% Constants
+DIM_1 = 50
+DIM_2 = 200
+DIM_3 = 500
+
+N_COMPONENTS = [50,100,200]
+KERNALS = ['linear', 'rbf', 'poly']
+
+Cs = []
+GAMMAs = []
+DEGREEs = []
+
+'''
+Data Extraction
+'''
+#%% #1
 
 # MNIST - 70,000 images (60k are for training) of handwritten digits from 0-9.
-# Each sample is a 28x28 grayscale image
-
 (X_train, Y_train), (X_test, Y_test) = mnist.load_data()
 (X_train_f, Y_train_f), (X_test_f, Y_test_f) = fashion_mnist.load_data()
+#plt.imshow(X_train[1])
+#plt.show()
+
+#%% #2
 #flatten all data
-X_train = X_train.reshape(X_train.shape[0] -1)
-X_test = X_test.reshape(X_test.shape[0], -1)
-Y_train = Y_train.reshape(Y_train.shape[0], -1)
-Y_test = Y_test.reshape(Y_test.shape[0], -1)
+X_train = X_train.reshape(X_train.shape[0], -1)
+X_tst = X_test.reshape(X_test.shape[0], -1)
 
-X_train_f = X_train_f.reshape(X_train_f.shape[0] -1)
+X_train_f = X_train_f.reshape(X_train_f.shape[0], -1)
 X_test_f = X_test_f.reshape(X_test_f.shape[0], -1)
-Y_train_f = Y_train_f.reshape(Y_train_f.shape[0], -1)
-Y_test_f = Y_test_f.reshape(Y_test_f.shape[0], -1)
 
-print('MNIST Dataset Shape:')
-print('X_train: ' + str(X_train.shape))
-print('Y_train: ' + str(Y_train.shape))
-print('X_test:  '  + str(X_test.shape))
-print('Y_test:  '  + str(Y_test.shape))
-
-plt.imshow(X_train_f[0])
-plt.show()
+'''
+Preprocessing Pipeline
+'''
+#%% 3.1 Standardize the flatten samples
+#TODO should we introduce Cross-validation?
+sc = StandardScaler()
+X_train_std = sc.fit_transform(X_train)
 
 
+#%% 3.2 Dimensionality Reduction
+#PCA
+#LDA
 
-#%% 
+
+#%%
+
+
+
+
+
---
+++ report.md
@@ -0,0 +1,6 @@
+# Midterm Project: Machine Learning Pipeline, Hyperparamter tuning, and Ensemble Learning
+## Miles Nordwall, Nathan Nail
+### Task 1
+### Task 2
+### Task 3
+### Task 4
