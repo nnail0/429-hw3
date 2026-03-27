eca3d0685f79de8bf626d240cf03844d1c1704cf
Nathan Nail <nailcnathan@gmail.com>
2026-03-27 13:38:28 -0600

    saving changes

---
+++ p4.py
@@ -0,0 +1,9 @@
+# Write a program that trains a finite set of SVCs using bootstrap aggregating. Please use at least 8
+# models. The training dataset should be divided into multiple disjoint sets and each individual model is trained
+# based on a subset. The final prediction for an image is obtained via voting. Please use the three kernels
+# along with their best hyperparameters you found in the previous task. Please do not use sklearn.ensemble.
+
+
+# For Task 4, please give a comparison between a single SVC (result of Task 3) and the result of bootstrap
+# aggregating for each kernel. The comparisons should be based on training time and test error.
+
